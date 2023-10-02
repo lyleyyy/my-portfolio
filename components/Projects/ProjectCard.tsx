7d341c32acfae6c9b06f5b176365817f104b3350
lyleyang <lyletwro@gmail.com>
2023-10-02 14:40:02 +1000

    fixed react-icons rx not Rx

--- components/Projects/ProjectCard.tsx
+++ components/Projects/ProjectCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { DiGithubBadge } from "react-icons/di";
-import { RxExternalLink } from "react-icons/Rx";
+import { RxExternalLink } from "react-icons/rx";
 import SkillItem from "../About/SkillItem";
 
 interface ProjectCardProps {
